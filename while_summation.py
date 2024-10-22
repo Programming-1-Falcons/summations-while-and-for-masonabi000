1162feadfa3dce9d3c8ca74c806c53f80a5a464b
masonabi000 <masonabi000@alpinesd.org>
2024-10-22 09:20:01 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,12 @@
 #while summation code following directions from Readme
+number = int(input("Pick a number: "))
+x = 1
+total = 0
+while x <= number:
+  print(x)
+  x += 1
+  total += x-1
+
+
+
+print ("= " + str(total))
